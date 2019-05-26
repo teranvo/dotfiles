2ecd64329ae27a7df744c20541aee35031a2c2a1
Wilfredo Gonzalez <wilfredogonzalezcarvajal@gmail.com>
2019-05-26 17:21:45 -0400

    ADD theme icon to desktop

--- debian/desktop.sh
+++ debian/desktop.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 apt install plank
 apt install xarchiver
+apt install arc-theme
+apt install moka-icon-theme
+rm -rf /usr/share/icons/Arc
+git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme && ./autogen.sh --prefix=/usr && make install
+rm -rf arc-icon-theme
